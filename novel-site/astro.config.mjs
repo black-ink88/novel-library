cde33beda92bf793a435a574ed15b5483092baa5
Aphelios <aphelios@Apheliosui-MacBookAir.local>
2026-07-11 15:35:16 +0900

    Update Pages owner to black-ink88

--- novel-site/astro.config.mjs
+++ novel-site/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://d0742534.github.io",
+  site: "https://black-ink88.github.io",
   base: process.env.BASE_PATH ?? "/",
   output: "static"
 });
